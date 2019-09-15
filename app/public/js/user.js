6fc42edd7f338517b1131043ca05203c61abee55
Kaitlyn Daanen <kddaanen@iu.edu>
2019-09-15 19:52:44 -0400

    Code Syntax has been added-still needs troubleshooting
    I have added all of the code syntax and created the two a new js file (user.js). II still can't get my file to connect properly to the JSON file, so I am working on troubleshooting that still. I think some syntax or wording is incorrect.

---
+++ app/public/js/user.js
@@ -0,0 +1,23 @@
+var randomuser = new Vue ({
+  el: '#userinfo',
+  data: {
+    users: {
+      name:"",
+      dob:"",
+      age:"",
+      email:"",
+      nat:"",
+      picture:""
+    }
+},
+methods: {
+  fetchUser() {
+    fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(json => {randomuser.users = json.results[0]});
+  }
+},
+created(){
+    this.fetchUser()
+  }
+});
